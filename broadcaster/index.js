42e865398d09fd84cdf060ba208c509287bf7f56
pacozavala <pako.zavala@gmail.com>
2022-09-06 15:29:16 +0300

    Fetching discord from process.env

--- broadcaster/index.js
+++ broadcaster/index.js
@@ -2,12 +2,11 @@ require('dotenv').config()
 const NATS = require('nats')
 const natsSC = NATS.StringCodec()
 const NATS_URL = process.env.NATS_URL
+const DISCORD_URL = process.env.DISCORD_URL
 
 console.log({ NATS_URL })
 
 const axios = require('axios')
-const discord_url =
-  'https://discord.com/api/webhooks/1012335028254036099/JS8N_3lG6iGPLRORpz1mcOfIX5WzPJ5JrUJryyFm2_nOCOwmP8qcBakK6YtfDm3DQLD-'
 
 console.log('Broadcaster started')
 NATS.connect({ servers: NATS_URL }).then(async conn => {
@@ -18,6 +17,6 @@ NATS.connect({ servers: NATS_URL }).then(async conn => {
     console.log(`Sending message: ${natsSC.decode(message.data)}`)
     console.log(natsSC.decode(message.data))
     const m_json = { 'content': natsSC.decode(message.data) }
-    await axios.post(discord_url, m_json)
+    await axios.post(DISCORD_URL, m_json)
   }
 })
